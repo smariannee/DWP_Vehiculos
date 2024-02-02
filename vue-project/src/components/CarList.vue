774c20bcb99a5c87914e23c98b0522a32ad60520
Marianne Santos <20213tn140@utez.edu.mx>
2024-02-02 16:15:05 -0600

    pagination

--- vue-project/src/components/CarList.vue
+++ vue-project/src/components/CarList.vue
@@ -27,7 +27,19 @@
                         </b-row>
                         <b-row>
                             <b-col cols="12">
-                                <b-table :items="cars" responsive="sm" class="px-3" small striped hover></b-table>                                
+                                <b-table id="my-table" :items="cars" :per-page="perPage" :current-page="currentPage"
+                                    responsive="sm" class="px-3" small stripped hover></b-table>
+                            </b-col>
+                            <b-col cols="12">
+                                <label for="perPage">Selecciona la cantidad de registros que deseas mostrar:</label>
+                                <b-form-select :options="pageOptions" v-model="perPage"
+                                    class="mx-3 my-3 p-1 select"></b-form-select>
+                            </b-col>
+                            <b-col cols="12" class="text-center">
+                                <b-pagination v-model="currentPage" :total-rows="rows" :per-page="perPage"
+                                    aria-controls="my-table" align="center"></b-pagination>
+                                <small class="mt-3">Página actual: {{ currentPage }}</small><br />
+                                <small class="mt-3">Total de vehículos: {{ rows }}</small>
                             </b-col>
                         </b-row>
                     </b-card-body>
@@ -41,6 +53,9 @@
 export default {
     data() {
         return {
+            perPage: 5,
+            currentPage: 1,
+            pageOptions: [5, 10, 15, 20, 25, 30],
             cars: [
                 {
                     brand: 'Nissan',
@@ -59,6 +74,48 @@ export default {
                     model: 'Fiesta',
                     year: 2017,
                     series: '1357924680'
+                },
+                {
+                    brand: 'Volkswagen',
+                    model: 'Jetta',
+                    year: 2016,
+                    series: '2468135790'
+                },
+                {
+                    brand: 'Toyota',
+                    model: 'Corolla',
+                    year: 2015,
+                    series: '9876543210'
+                },
+                {
+                    brand: 'Honda',
+                    model: 'Civic',
+                    year: 2014,
+                    series: '0123456789'
+                },
+                {
+                    brand: 'Mazda',
+                    model: '3',
+                    year: 2013,
+                    series: '9876543210'
+                },
+                {
+                    brand: 'Kia',
+                    model: 'Rio',
+                    year: 2012,
+                    series: '0123456789'
+                },
+                {
+                    brand: 'Hyundai',
+                    model: 'Accent',
+                    year: 2011,
+                    series: '9876543210'
+                },
+                {
+                    brand: 'Audi',
+                    model: 'A3',
+                    year: 2010,
+                    series: '0123456789'
                 }
             ],
             items: [
@@ -73,11 +130,21 @@ export default {
             ],
         }
     },
+    
+  computed: {
+    rows() {
+      return this.cars.length
+    }
+  },
 }
 </script>
   
 <style scoped>
 .icon {
     margin-left: 10px;
+}
+.select {
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
 }
 </style>
